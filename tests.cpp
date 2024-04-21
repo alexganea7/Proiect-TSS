b77ea5335378db29354547399fe8c04614a02e3d
alexganeam <ganeaalex222@gmail.com>
2024-04-21 16:31:53 +0300

    test

---
+++ tests.cpp
@@ -0,0 +1,14 @@
+#include "whattotest.cpp"
+#include <gtest/gtest.h>
+
+TEST(maximTest, marePrimul) { 
+    ASSERT_EQ(1, maxim(1,0));
+    ASSERT_EQ(1, maxim(0,1));
+}
+
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ whattotest.cpp
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <iostream>
+
+int maxim(int a, int b) {
+    if (a > b) {
+        return a;
+    }
+    else
+        return b;
+}
