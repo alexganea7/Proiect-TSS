0f2422804d8f5d72439b33826f63489a448d6822
Alex Ganea <138498475+alexganea7@users.noreply.github.com>
2024-04-21 20:02:18 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -5,9 +5,6 @@ TEST(maximTest, marePrimul) {
     ASSERT_EQ(1, maxim(1,0));
     ASSERT_EQ(1, maxim(0,1));
 }
-
-
-
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
